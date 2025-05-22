a616e1ab54c4a5aa8a9bddbccdb89dd998ac02b3
Putri Oktatriani <101786412+putriokta@users.noreply.github.com>
2025-05-21 14:22:38 -1200

    Update app2.py

--- app.py
+++ app.py
@@ -157,7 +157,6 @@ def index():
 
 # === JALANKAN ===
 if __name__ == '__main__':
-    threading.Thread(target=loop_monitoring).start()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
