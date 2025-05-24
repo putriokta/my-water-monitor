ce8a7f80bb469336d577a83fd95398556226b232
Putri Oktatriani <101786412+putriokta@users.noreply.github.com>
2025-05-23 17:49:41 -1200

    Update app.py

--- app.py
+++ app.py
@@ -176,6 +176,6 @@ def index():
 
 # === JALANKAN APP ===
 if __name__ == '__main__':
-    # loop_monitoring()
+    loop_monitoring()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, use_reloader=False)
